9f94525b37ea0f651d43350bb208df8ef2e123b1
ssegevv <segevb97@Gmail.com>
2024-12-16 13:05:21 +0200

    delete garbage and add keys

--- src/components/cardArray.js
+++ src/components/cardArray.js
@@ -4,7 +4,7 @@ import Card from './card';
 const cardArray = ({robots}) => {
    const cardComponent = robots.map((user, i) => {
       return (
-        <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
         )
       }
     )
